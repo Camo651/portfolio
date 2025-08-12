5a90751c19e74994746c058cf81d675a5800e419
Matt <47070087+Camo651@users.noreply.github.com>
2025-08-12 14:56:11 -0400

    init

---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const LandingPage = () => {
+
+    return (
+        <div>
+            <h1 style={{
+                width: '100%',
+                fontWeight: 800,
+                fontSize: '16vw',
+                scale: 1.03,
+                margin: 0,
+                padding: 0,
+            }}>Matt Hagger</h1>
+            <div >
+            
+            </div>
+        </div>
+    );
+};
+
+export default LandingPage;
